7f7137efac37a5c2242eac2de1af6cf619ea5ae3
pokorra <patrycja.pokora@gmail.com>
2020-12-17 23:39:26 +0100

    main section with animated slider

--- js/app.js
+++ js/app.js
@@ -5,8 +5,29 @@ hamburger.addEventListener("click", ()=>{
     if (menu.className === "hidden") {
         menu.className = menu.className.replace("hidden", "open");
         hamburger.classList.add("show-menu");
+        noScrolling();
     } else {
         menu.className = menu.className.replace("open", "hidden");
         hamburger.classList.remove("show-menu");
+        noScrolling();
+    }
+})
+
+function noScrolling(){
+    if (document.documentElement.clientWidth < 651){
+        if (document.body.classList.contains("block")) {
+            document.body.classList.add("block");
+        } else {
+            document.body.classList.remove("block");
+        }
+    }
+}
+
+//closing menu after clicking on the link
+menu.addEventListener("click", e => {
+    if (e.target.tagName === "A") {
+        menu.className = menu.className.replace("open", "hidden");
+        hamburger.classList.remove("show-menu");
+        noScrolling();
     }
 })
