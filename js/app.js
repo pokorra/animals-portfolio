0b805c8a8e66742c4c610467072b5ef64bf8ffbb
pokorra <patrycja.pokora@gmail.com>
2020-12-31 00:52:43 +0100

    scrolling added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+function Smooth(from, to){
+    from.addEventListener('click', ()=>{
+        to.scrollIntoView({behavior: "smooth"});
+        console.log(`${from} this one to ${to}`);
+    })
+};
+
+Smooth(document.querySelector('#to-contact'), document.querySelector('#contact'));
+Smooth(document.querySelector('#to-team'), document.querySelector('#team'));
+Smooth(document.querySelector('#to-about'), document.querySelector('#about'));
+Smooth(document.querySelector('#to-top'), document.querySelector('#site-top'));
 
 const hamburger = document.getElementById("hamburger");
 const menu = document.getElementById("menu");
@@ -47,5 +58,4 @@ function toggleActive(e){
 }
 
 memberContainer.forEach(member => member.addEventListener('click', toggleExtend));
-
 memberContainer.forEach(member => member.addEventListener('transitionend', toggleActive));
