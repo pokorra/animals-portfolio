217c60a571649c1b98ab09c39812ccc3babaa5fe
pokorra <patrycja.pokora@gmail.com>
2021-01-04 00:15:35 +0100

    added some pseudo elements and preloader

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
+//loader
+const loader = document.querySelector('.loader-wrapper')
+window.addEventListener('beforeunload', ()=>{
+    loader.style.display = 'flex';
+});
+
+window.addEventListener('load', ()=>{
+    loader.style.display = 'none';
+})
+
 //dark mode starting
-const darkBtn = document.querySelector('.toggle-mode');
 const body = document.querySelector('body');
+const darkBtn = document.querySelector('.toggle-mode');
 darkBtn.addEventListener('click', function(){
     body.classList.toggle('dark');
     darkBtn.classList.toggle('clicked');
-
     if (darkBtn.classList.contains('clicked')){
         darkBtn.innerHTML = "light mode";
     } else { darkBtn.innerHTML = "dark mode";}
@@ -35,7 +44,6 @@ hamburger.addEventListener("click", ()=>{
         noScrolling();
     }
 })
-
 function noScrolling(){
     if (document.documentElement.clientWidth < 651){
         if (document.body.classList.contains("block")) {
@@ -59,7 +67,6 @@ menu.addEventListener("click", e => {
 const main = document.querySelector("main");
 const upBtn = document.querySelector('#to-top')
 window.addEventListener("scroll", showUpArrow);
-
 function showUpArrow() {
   if (window.scrollY > (main.offsetHeight)) {
     upBtn.classList.remove("hidden");
@@ -68,11 +75,8 @@ function showUpArrow() {
   }
 }
 
-
-
 // who we are section - extending pictures
 const memberContainer = document.querySelectorAll('.team-member');
-
 function toggleExtend(){
     this.classList.toggle('extended');
 }
@@ -81,13 +85,11 @@ function toggleActive(e){
         this.classList.toggle('open-active')
     };
 }
-
 memberContainer.forEach(member => member.addEventListener('click', toggleExtend));
 memberContainer.forEach(member => member.addEventListener('transitionend', toggleActive));
 
 //form - hiding label on typing
 const form = document.querySelector('form');
-
 form.addEventListener('keyup', labelOut);
 function labelOut(e){
         if (!e.target.value) {
